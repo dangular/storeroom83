5ef44089881f2f2298c6dc1f050bb1fe5ed40887
dangular <dan@dangular.com>
2014-04-06 11:00:19 -0400

    fixed issue where winston logger was failing if path to log file didn't exist. Also, added back 'logs' to .gitignore.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var app = require('./server'),
     config = require('./lib/configuration'),
-    db = require('./lib/model/db');
+    db = require('./lib/model/db'),
+    fs = require('fs');
 
 app.set('port', config.get("express:port"));
+
+// winston won't create directory, create the logging path if it doesn't exist
+fs.exists(config.get("logger:path"), function(exists) {
+    if (!exists) {
+        fs.mkdir(config.get("logger:path"), function(err) {
+            if (err) throw err;
+            console.log("Created path for logger: "+config.get("logger:path"));
+        });
+    }
+});
+
 
 app.listen(app.get('port'), function(){
     console.log("Server listening on port "+ app.get('port'));
