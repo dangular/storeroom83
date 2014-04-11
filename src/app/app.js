7f4b666bd4e0ae338e8b336f229ef60b4990a684
dangular <dan@dangular.com>
2014-04-11 13:54:58 -0400

    Added confirmation and alert (inline and growl) services. Applied to delete storeroom feature.

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,11 @@
  * Created by dhayes on 3/31/14.
  * app.js
  */
-angular.module('app', ['ui.router','ui.bootstrap','inventory'])
+angular.module('app', ['ngAnimate', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'angular-growl', 'inventory'])
+
+    .config(['growlProvider', function(growlProvider){
+        growlProvider.globalEnableHtml(true);
+    }])
 
     .run(['$rootScope','$state', 'sidebarService', function($rootScope, $state, sidebarService){
 
@@ -16,6 +20,8 @@ angular.module('app', ['ui.router','ui.bootstrap','inventory'])
         });
 
     }])
+
+    .constant("alertAutoCloseTimeout", 5000)
 
     .service('sidebarService', function() {
         return {
@@ -32,5 +38,78 @@ angular.module('app', ['ui.router','ui.bootstrap','inventory'])
     .controller('AppController', ['$scope', 'sidebarService', function($scope, sidebarService){
 
         $scope.sidebarService = sidebarService;
+
+    }])
+
+    .service('AlertService', ['$rootScope', '$timeout', 'growl', 'alertAutoCloseTimeout', function($rootScope, $timeout, growl, alertAutoCloseTimeout) {
+        $rootScope.inlineAlerts = [];
+
+        $rootScope.closeInLineAlert = function (index) {
+            $rootScope.inlineAlerts.splice(index, 1);
+        };
+
+        var growlOptions = {};
+
+        return {
+            growl: function(type, message, autoClose) {
+                if (autoClose) {
+                    growlOptions.ttl = alertAutoCloseTimeout;
+                }
+                switch(type) {
+                    case 'success':
+                        growl.addSuccessMessage(message, growlOptions);
+                        break;
+                    case 'info':
+                        growl.addInfoMessage(message, growlOptions);
+                        break;
+                    case 'warn':
+                        growl.addWarnMessage(message, growlOptions);
+                        break;
+                    case 'error':
+                        growl.addErrorMessage(message, growlOptions);
+                        break;
+                    default:
+                        growl.addInfoMessage(message, growlOptions);
+                }
+
+            },
+            inline: function(type, message, autoClose) {
+                var alert = {type: type, msg: message};
+                $rootScope.inlineAlerts.push(alert);
+
+                if (autoClose) {
+                    $timeout(function() {
+                        $rootScope.inlineAlerts = _.without($rootScope.inlineAlerts, alert);
+                    }, alertAutoCloseTimeout);
+                }
+            }
+        };
+    }])
+
+    .service('ConfirmService', ['$modal', function($modal){
+
+        return {
+            confirm: function(message, confirm, cancel) {
+
+                var modalInstance = $modal.open({
+                    templateUrl: '/partials/shared/confirm',
+                    controller: ['$scope', '$modalInstance', function($scope, $modalInstance){
+                        $scope.body = message;
+
+                        $scope.ok = function () {
+                            $modalInstance.close(true);
+                        };
+
+                        $scope.cancel = function () {
+                            $modalInstance.dismiss(false);
+                        };
+
+                    }]
+                });
+
+                modalInstance.result.then(confirm, cancel);
+
+            }
+        };
 
     }]);
--- src/app/inventory/controllers.js
+++ src/app/inventory/controllers.js
@@ -2,14 +2,23 @@
  * Created by: dhayes on 4/9/14.
  * Filename: storerooms.controller
  */
-angular.module('inventory.controllers', [])
+angular.module('inventory.controllers', ['ui.bootstrap'])
 
-    .controller('StoreroomController', ['$scope', 'storerooms', 'StoreroomRepository', function($scope, storerooms, repository) {
+    .controller('StoreroomController', ['storerooms', '$scope', 'ConfirmService', 'AlertService', 'StoreroomRepository', function(storerooms, $scope, ConfirmService, AlertService, repository) {
         $scope.storerooms = storerooms;
 
         $scope.remove = function(storeroom) {
-            repository.remove(storeroom).then(function(){
-                $scope.storerooms = _.without($scope.storerooms, storeroom);
+
+            ConfirmService.confirm("Are you sure you want to delete storeroom "+storeroom.name, function () {
+                repository.remove(storeroom).then(function(){
+                    $scope.storerooms = _.without($scope.storerooms, storeroom);
+                    AlertService.growl('success', 'Storeroom '+storeroom.name+' successfully removed.', true);
+                }, function(err){
+
+                });
+            }, function(){
+                AlertService.growl('info', 'Delete canceled', true);
             });
+
         };
     }]);
